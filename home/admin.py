04395843f7ea01290c84de37ff582dca51fdfa96
nareshrajesh <nareshrajesh787@gmail.com>
2025-01-08 22:29:02 -0500

    1/8/25v2

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Assignment
+from .models import Assignment, Review
 # Register your models here.
 admin.site.register(Assignment)
+admin.site.register(Review)
